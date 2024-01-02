441af10535d8900e3cd9a8386ae05445c95a1f9a
Poonam Magar <102014652+PoonamMagar123@users.noreply.github.com>
2024-01-02 23:42:44 +0530

    Add files via upload

---
+++ GUI/tut4.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+imaginary_root = Tk()
+
+imaginary_root.geometry("444x555")
+
+imaginary_root.minsize(200,100)
+
+# imaginary_root.minsize(1200,100)
+
+variable_t = Label(text="THIS is Text and this is GUI")
+variable_t.pack()
+imaginary_root.mainloop()
+
---
+++ GUI/tut4_task.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+imaginary_root = Tk()
+
+imaginary_root.geometry("730x434")
+
+imaginary_root.minsize(200,100)
+
+# imaginary_root.minsize(1200,100)
+
+variable_t = Label(text="Welcome to GUI")
+variable_t.pack()
+imaginary_root.mainloop()
+
